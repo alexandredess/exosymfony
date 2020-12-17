2c17f97f27358e3109693399ec0405f1f6012366
alexandredess <alexandre.dessoly@laposte.net>
2020-12-17 18:50:48 +0100

    creation du login

--- my_project_name/config/bundles.php
+++ my_project_name/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
